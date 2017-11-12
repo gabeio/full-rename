e5cf8084881fedc82da9a4fb4adfe3bcbdbccc7d
Gabe De Luca <me@gabe.io>
2017-11-11 22:28:44 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Full Rename
+
+First of all this focusing solely on OS X/macOS (this may apply to linux somewhat).
+
+Follow the basics of renaming a user from Apple's own documentation:
+https://support.apple.com/en-us/HT201548
+
+If you are not a power user/developer you are likely done. If you are a power user/developer I suggest you continue.
+
+
+## After renaming the user account and the user's home folder.
+
+run: `python relink.py -u <the old username>`
---
+++ relink.py
@@ -0,0 +1,40 @@
+import argparse
+import locale
+import subprocess
+import re
+
+def relink(olduser):
+    rex = re.compile(r"\d\d\:\d\d\ (.*)\ \-\>\ (.*)\n")
+    # find the new user
+    cmd = ["whoami"]
+    newuser,error = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
+    # newuser = subprocess.check_output(cmd)
+    newuser = newuser.replace("\n","")
+    print(newuser)
+    # find all old (soft) links
+    cmd = ['/bin/bash', '-c']
+    cmd.append('find / -type l -ls 2>/dev/null | grep {olduser}'.format(olduser=olduser))
+    print(cmd)
+    found, error = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
+    print(found)
+    groups = rex.findall(found)
+    print(groups)
+    # go through all links
+    for item in groups:
+        dest = item[1].replace(olduser, newuser)
+        # relink them to the new user
+        cmd = ['/bin/bash', '-c']
+        cmd.append('ln -sf \"{origin}\" \"{destination}\"'.format(origin=item[0], destination=dest))
+        print(cmd)
+        out, error = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
+        print(out)
+        print(error)
+
+if __name__ == '__main__':
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+    parser = argparse.ArgumentParser(description='Re-links olduser home directory to current user\'s home directory.')
+    parser.add_argument('-u', '--user', dest='olduser', required=True,
+                        help='The old user account which will need to relinked.')
+    args = parser.parse_args()
+
+    relink(args.olduser)
